935d24f104f789364f73a5f78b742b17f71ba792
Diego Coronado <dcoronado@able.co>
2022-07-24 16:54:57 +0900

    fix sidebar bug

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const App = () => {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <div className="w-72 flex sidebar dark:bg-secondary-dark-bg bg-white">
               <Sidebar />
             </div>
           ) : (
